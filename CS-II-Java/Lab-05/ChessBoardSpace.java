5d2b8e1281c2640440a8697a8d8f0707e9f2f7b3
TheNotoriousWMB <TheNotoriousWMB@users.noreply.github.com>
2016-03-20 18:40:02 -0500

    Create ChessBoardSpace.java

---
+++ CS-II-Java/Lab-05/ChessBoardSpace.java
@@ -0,0 +1,61 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class ChessBoardSpace {
+	int x, y;
+	int width, height;
+	Color color;
+
+	public ChessBoardSpace(int x, int y, int width, int height, Color color) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.color = color;
+	}
+
+	public void draw(Graphics g) {
+		g.setColor(color);
+		g.fillRect(x, y, width, height);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+}
